6341d7f95cb0464c48ceba5b0d84170f5c6485ee
nbwns <nicolas.bauwens@gmail.com>
2016-03-02 12:41:05 +0100

    AJAXified via $http service

--- ponyclub.js
+++ ponyclub.js
@@ -1,17 +1,27 @@
 var ponyclubAppModule = angular.module("ponyclubApp",[]);
 
-ponyclubAppModule.controller("PonyListController", function(){
+ponyclubAppModule.controller("PonyListController", function($scope, $http){
     var ponyclub = this;
-    ponyclub.ponys = [
+    /*ponyclub.ponys = [
         {name: "Rainbow Dash", description: "Double rainbow !"},
         {name: "Pinkie Pie", description: "If you love pink you'll love her"}
-    ];
+    ];*/
+    
+    $http.get('https://node-test-nbwns.c9.io/ponies').success(function(data) {
+        ponyclub.ponies = data;
+    });
     
     ponyclub.add = function(){
         var newPony = {name: ponyclub.ponyname, description: ponyclub.ponydescription};
-        ponyclub.ponys.push(newPony);
-        ponyclub.ponyname = '';        
-        ponyclub.ponydescription = '';
+         $http.post('https://node-test-nbwns.c9.io/pony', newPony)
+         .success(function(data) {
+            ponyclub.ponies.push(newPony);
+            ponyclub.ponyname = '';        
+            ponyclub.ponydescription = '';
+        })
+        .error(function(){
+            alert("error");
+        });
     };
 
 });
